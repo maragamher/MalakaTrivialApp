df45c4d4d5114f1b4e349171c48a658fc1b2a376
Mara <maragambero.gmail.com>
2024-05-28 22:36:41 +0200

    Proyecto vacío + carpetas Model/View/Control

--- app/src/main/java/com/politecnicomalaga/malakatrivialapp/view/MainActivity.java
+++ app/src/main/java/com/politecnicomalaga/malakatrivialapp/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.politecnicomalaga.malakatrivialapp;
+package com.politecnicomalaga.malakatrivialapp.view;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.politecnicomalaga.malakatrivialapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
