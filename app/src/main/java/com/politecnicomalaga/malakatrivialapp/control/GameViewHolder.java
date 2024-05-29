3130e2867e1f455a635533ba96352936c29102a3
Sergio Soriano Narbona <114468773+sergiosorianoo@users.noreply.github.com>
2024-05-29 08:43:06 +0200

    Add files via upload

---
+++ app/src/main/java/com/politecnicomalaga/malakatrivialapp/control/GameViewHolder.java
@@ -0,0 +1,40 @@
+package com.politecnicomalaga.malakatrivialapp.control;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.politecnicomalaga.malakatrivialapp.R;
+import com.politecnicomalaga.trivialapp.R;
+
+public class GameViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView tvNombre, tvDescripcion;
+
+    public GameViewHolder(@NonNull View itemView, GameRVAdapter adapter) {
+        super(itemView);
+
+        tvNombre = itemView.findViewById(R.id.tvNombre);
+        tvDescripcion = itemView.findViewById(R.id.tvDescrip);
+
+    }
+
+    public TextView getTvNombre() {
+        return tvNombre;
+    }
+
+    public void setTvNombre(TextView tvNombre) {
+        this.tvNombre = tvNombre;
+    }
+
+    public TextView getTvDescripcion() {
+        return tvDescripcion;
+    }
+
+    public void setTvDescripcion(TextView tvDescripcion) {
+        this.tvDescripcion = tvDescripcion;
+    }
+}
